9b44b42e41bd3a6f2cc8e3d83526a979974b67cf
molnarland <rrmolnar@gmail.com>
2016-09-30 23:48:58 +0100

    add included files

---
+++ script/landing.js
@@ -0,0 +1,68 @@
+window.onscroll = function ()
+{
+	var scroll = scrollTop();
+
+	setElementOpacity('#menu .molnarland-text', scroll / 200);
+	setElementOpacity('#welcome .molnarland-text', 1 - (scroll / 200));
+
+	if (scroll > 100) 
+	{
+		//TODO make paralax show/hide with #menu and #welcome .molnarland-text
+
+		addString('#menu', ' top-fix')
+	}
+	else
+	{
+		addString('#menu .molnarland-text', ' hide');
+		replaceString('#welcome .molnarland-text', 'hide');
+
+		replaceString('#menu', 'top-fix');
+	}
+}
+
+function scrollTop()
+{
+	return (document.body.scrollTop > 0)
+		? document.body.scrollTop
+		: document.documentElement.scrollTop;
+}
+
+function substring(value, string) 
+{
+	return value.indexOf(string) !== -1;
+}
+
+function replaceString(selector, string)
+{
+	var cssClassName = document.querySelector(selector).className;
+	if (substring(cssClassName, string)) 
+	{
+		document.querySelector(selector).className = cssClassName.replace(string, '');
+	}
+}
+
+function addString(selector, string)
+{
+	if (!substring(document.querySelector(selector).className, string)) 
+	{
+		document.querySelector(selector).className += string;
+	}
+}
+
+function checkSelector(selector)
+{
+	if (!document.querySelector(selector))
+	{
+		throw new Error(selector + ' selector isn\'t exist');
+	}
+
+	return true;
+}
+
+function setElementOpacity(selector, opacity) 
+{
+	if (checkSelector(selector)) 
+	{
+		document.querySelector(selector).style.opacity = opacity;
+	}
+}
