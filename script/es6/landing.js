e307024921916c85480403b570f38d2b3755f255
molnarland <rrmolnar@gmail.com>
2016-10-16 14:30:59 +0100

    smooth scroll implemented

--- script/es6/landing.js
+++ script/es6/landing.js
@@ -10,7 +10,10 @@ let welcomeTextPositionIsLeft = 'center',
 
 const welcomeTextId = '#welcome-text',
     photoOfMeId = '#photo-of-me',
-	rastaTextClass = '.rasta-text';
+	rastaTextClass = '.rasta-text',
+	navbarHeight = document.querySelector('#navbar').clientHeight;
+
+console.log(navbarHeight);
 
 let windowWidth = window.innerWidth;
 
@@ -22,6 +25,11 @@ document.querySelector('body').onresize = () =>
     windowWidth = window.innerWidth;
 
     onloadAndOnresizeFunctions();
+};
+
+document.querySelector('#answer').onclick = () =>
+{
+	smoothScroll('#iam');
 };
 
 window.onscroll = () =>
@@ -277,9 +285,25 @@ function inBirmingham()
 
 function scrollTop()
 {
-	return (document.body.scrollTop > 0)
-		? document.body.scrollTop
-		: document.documentElement.scrollTop;
+	// Firefox, Chrome, Opera, Safari
+	if (self.pageYOffset)
+	{
+		return self.pageYOffset;
+	}
+
+	// Internet Explorer 6 - standards mode
+	if (document.documentElement && document.documentElement.scrollTop)
+	{
+		return document.documentElement.scrollTop
+	}
+
+	// Internet Explorer 6, 7 and 8
+	if (document.body.scrollTop)
+	{
+		return document.body.scrollTop;
+	}
+
+	return 0;
 }
 
 function substring(value, string) 
@@ -330,4 +354,86 @@ function setElementOpacity(selector, opacity)
 function randomNumber(max, min = 0)
 {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function smoothScroll(selector)
+{
+	let startY = scrollTop();
+
+	elementTop(selector, (position) =>
+	{
+		let stopY = position - navbarHeight,
+			distance = stopY > startY ? stopY - startY : startY - stopY;
+
+		if (distance < 100)
+		{
+			scrollTo(0, stopY); return;
+		}
+
+		let speed = Math.round(distance / 100);
+		if (speed >= 20)
+		{
+			speed = 20;
+		}
+
+		let step = Math.round(distance / 200),
+			leapY = stopY > startY ? startY + step : startY - step,
+			timer = 0;
+
+		if (stopY > startY)
+		{
+			for (let i = startY; i < stopY; i += step)
+			{
+				setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+
+				leapY += step;
+				if (leapY > stopY)
+				{
+					leapY = stopY;
+				}
+
+				timer++;
+			}
+
+			return;
+		}
+
+		for (let i = startY; i > stopY; i -= step)
+		{
+			setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+
+			leapY -= step;
+			if (leapY < stopY)
+			{
+				leapY = stopY;
+			}
+
+			timer++;
+		}
+	});
+}
+
+
+function elementTop(selector, next)
+{
+	checkSelector(selector, () =>
+	{
+		const element = document.querySelector(selector);
+		let y = element.offsetTop,
+			node = element;
+		
+		while (node.offsetParent && node.offsetParent != document.body)
+		{
+			node = node.offsetParent;
+			y += node.offsetTop;
+		}
+
+
+		if (typeof next === 'function')
+		{
+			return next(y);
+		}
+
+		return y;
+	});
 }
--- script/landing.js
+++ script/landing.js
@@ -11,7 +11,10 @@ var welcomeTextPositionIsLeft = 'center',
 
 var welcomeTextId = '#welcome-text',
     photoOfMeId = '#photo-of-me',
-    rastaTextClass = '.rasta-text';
+    rastaTextClass = '.rasta-text',
+    navbarHeight = document.querySelector('#navbar').clientHeight;
+
+console.log(navbarHeight);
 
 var windowWidth = window.innerWidth;
 
@@ -22,6 +25,10 @@ document.querySelector('body').onresize = function () {
 	windowWidth = window.innerWidth;
 
 	onloadAndOnresizeFunctions();
+};
+
+document.querySelector('#answer').onclick = function () {
+	smoothScroll('#iam');
 };
 
 window.onscroll = function () {
@@ -235,7 +242,22 @@ function inBirmingham() {
 }
 
 function scrollTop() {
-	return document.body.scrollTop > 0 ? document.body.scrollTop : document.documentElement.scrollTop;
+	// Firefox, Chrome, Opera, Safari
+	if (self.pageYOffset) {
+		return self.pageYOffset;
+	}
+
+	// Internet Explorer 6 - standards mode
+	if (document.documentElement && document.documentElement.scrollTop) {
+		return document.documentElement.scrollTop;
+	}
+
+	// Internet Explorer 6, 7 and 8
+	if (document.body.scrollTop) {
+		return document.body.scrollTop;
+	}
+
+	return 0;
 }
 
 function substring(value, string) {
@@ -277,4 +299,71 @@ function randomNumber(max) {
 	var min = arguments.length <= 1 || arguments[1] === undefined ? 0 : arguments[1];
 
 	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function smoothScroll(selector) {
+	var startY = scrollTop();
+
+	elementTop(selector, function (position) {
+		var stopY = position - navbarHeight,
+		    distance = stopY > startY ? stopY - startY : startY - stopY;
+
+		if (distance < 100) {
+			scrollTo(0, stopY);return;
+		}
+
+		var speed = Math.round(distance / 100);
+		if (speed >= 20) {
+			speed = 20;
+		}
+
+		var step = Math.round(distance / 200),
+		    leapY = stopY > startY ? startY + step : startY - step,
+		    timer = 0;
+
+		if (stopY > startY) {
+			for (var i = startY; i < stopY; i += step) {
+				setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+
+				leapY += step;
+				if (leapY > stopY) {
+					leapY = stopY;
+				}
+
+				timer++;
+			}
+
+			return;
+		}
+
+		for (var _i2 = startY; _i2 > stopY; _i2 -= step) {
+			setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+
+			leapY -= step;
+			if (leapY < stopY) {
+				leapY = stopY;
+			}
+
+			timer++;
+		}
+	});
+}
+
+function elementTop(selector, next) {
+	checkSelector(selector, function () {
+		var element = document.querySelector(selector);
+		var y = element.offsetTop,
+		    node = element;
+
+		while (node.offsetParent && node.offsetParent != document.body) {
+			node = node.offsetParent;
+			y += node.offsetTop;
+		}
+
+		if (typeof next === 'function') {
+			return next(y);
+		}
+
+		return y;
+	});
 }
